90111c455a7e79fcfcb13f7cc09978e58fee9344
reichiq <khan.770977@gmail.com>
2025-04-15 18:52:01 +0300

    latexocr_minimal.py

--- latexocr_minimal.py
+++ latexocr_minimal.py
@@ -1,11 +1,9 @@
-# latexocr_minimal.py
-
 import sys
 sys.path.append("/root/vandili/LaTeX-OCR")
 
 from pix2tex.cli import LatexOCR
 
-# ⚠️ без model_name и других аргументов, они сейчас не нужны
+
 model = LatexOCR()
 
 def latex_ocr(img):
