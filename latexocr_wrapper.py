8ba0570edd18eab4303b6ac5fb702eeb84932b96
reichiq <khan.770977@gmail.com>
2025-04-15 15:44:19 +0300

    latexocr_wrapper.py

--- latexocr_wrapper.py
+++ latexocr_wrapper.py
@@ -1,5 +1,5 @@
 import sys
 sys.path.append("/root/vandili/LaTeX-OCR")
 
-from pix2tex.cli import LatexOCR
-latex_ocr = LatexOCR()
+from pix2tex.model import LatexOCR
+latex_ocr = LatexOCR(model_name="models/gemini-2.5-pro-exp-03-25")
